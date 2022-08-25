8e134e7126dc6f6b800417c9ad03772850a7a2c2
M4rc1an0 <marciano.arantes1999@gmail.com>
2022-08-24 23:59:43 -0300

    inserindo redux

--- pokedex/src/App.js
+++ pokedex/src/App.js
@@ -1,10 +1,26 @@
+import { useState } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
 import './App.css';
 import Header from './components/Header/Header'
+import { changeUser, selectUser } from './redux/userSlice';
 
 function App() {
+  const [name, setName] = useState('')
+
+  const user = useSelector(selectUser)
+  const dispatch = useDispatch()
+
+  console.log(user, 'USER')
+
+  const handleLogin = () => {
+    dispatch(changeUser(name))
+  }
+
   return (
     <div className="App">
       <Header/>
+      <input type="text" placeholder='Name' onChange={(e) => setName(e.target.value)}/>
+      <button onClick={handleLogin}>Login</button>
     </div>
   );
 }
