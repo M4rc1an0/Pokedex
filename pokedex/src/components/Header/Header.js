05b89754daf45e679712f8c11ebb2ff30a88970e
Rogério Marciano <marciano.arantes1999@gmail.com>
2022-08-27 14:59:51 -0300

    Ajustando header

--- pokedex/src/components/Header/Header.js
+++ pokedex/src/components/Header/Header.js
@@ -1,10 +1,56 @@
-import './Header.css'
+import { useEffect } from 'react'
+import * as S from './styles'
 
-const Header = (type) => {
+import { changeUser, logout, selectUser } from '../../redux/userSlice';
+import { useState } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+
+const Header = () => {
+    const user = useSelector(selectUser)
+    const dispatch = useDispatch()
+
+    const [name, setName] = useState('')
+    const [userLogget, setUserLogged] = useState(user)
+
+    
+    console.log(userLogget, 'USER')
+  
+    const handleLogin = () => {
+      dispatch(changeUser(name))
+    }
+
+    const handleLogout = () => {
+        dispatch(logout())
+    }
+
+    useEffect(() => {
+        setUserLogged(user)
+    }, [user])
+
     return (
-        <div className='container'>
-            INICIO
-        </div>
+        <S.Container>
+            <S.SideSpaces>
+                <S.Column>
+                    <S.ImgLogo src='./pokemon.png'/>
+                </S.Column>
+                <S.Column>
+
+                </S.Column>
+                <S.Column>
+                {!userLogget.isLogged ? ( 
+                        <>
+                            <S.Input type="text" placeholder='Name' onChange={(e) => setName(e.target.value)}/>
+                            <button onClick={handleLogin}>Login</button>
+                        </>
+                    ) : (
+                        <>
+                            <button onClick={handleLogout}>Sair</button>
+                        </>
+                    )}
+                </S.Column>
+            </S.SideSpaces>
+        </S.Container>
+
     )
 }
 
---
+++ pokedex/src/components/Header/styles.js
@@ -0,0 +1,28 @@
+import styled from 'styled-components'
+
+export const Container = styled.div`
+    width: 100%;
+    height: 100px;
+    background-color: #c3cad7;
+    border-bottom: 5px solid #000;
+`
+
+export const SideSpaces = styled.div`
+    display: flex;
+    justify-content: space-around;
+    padding: 10px;
+`
+
+export const Column = styled.div`
+`
+
+export const ImgLogo = styled.img`
+    width: 140px;
+`
+
+export const Input = styled.input`
+    padding: 0 5px;
+    border: none;
+    border-bottom: 2px solid #000;
+    outline: none;
+`
